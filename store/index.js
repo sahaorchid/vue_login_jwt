864c327135d7ba5d500b302df8dfab66ab741041
sahaorchid <70687710+sahaorchid@users.noreply.github.com>
2021-12-15 16:17:02 +0530

    Update index.js

--- store/index.js
+++ store/index.js
@@ -5,9 +5,17 @@ export default createStore({
     user:{}
   },
   mutations: {
+    setUser(state,user){
+      state.user = user
+    }
   },
   actions: {
   },
   modules: {
+  },
+  getters:{
+    getUser(state){
+      return state.user
+    }
   }
 })
