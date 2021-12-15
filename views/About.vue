29dff8bf5dc57516f2f75ff017b23d5f6932ace3
sahaorchid <70687710+sahaorchid@users.noreply.github.com>
2021-12-15 16:18:36 +0530

    Update About.vue

--- views/About.vue
+++ views/About.vue
@@ -1,18 +1,23 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <p>{{userData}}</p>
+    <p>{{getUser}}</p>
   </div>
 </template>
 
 <script>
-import store from '../store/index'
+// import store from '../store/index'
+import { mapGetters } from 'vuex'
 export default {
   name: 'Home',
   data(){
     return{
-      userData: store.state.user
+      userData: ""
     }
-  }
+  },
+  // created(){
+  //   this.userData = store.state.user
+  // },
+  computed: mapGetters(['getUser'])
 }
 </script>
